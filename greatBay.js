41b43d6a1cac98e4dfffcbc770644f8ee6def3a3
Mitu Fikir <hannatefera2@gmail.com>
2019-09-05 21:20:47 -0700

    bid item function added

--- greatBay.js
+++ greatBay.js
@@ -2,21 +2,20 @@ var inquirer = require("inquirer");
 
 main();
 
-function main(){
-    inquirer.prompt([
-        {
-            name: "mainAction",
-            type: "list",
-            message: "What would you like to do?",
-            choices: ["POST an Item", "BID on an Item", "EXIT"]
+function main() {
+    inquirer.prompt([{
+        name: "mainAction",
+        type: "list",
+        message: "What would you like to do?",
+        choices: ["POST an Item", "BID on an Item", "EXIT"]
 
-        }
-    ]).then(answers => {
+    }]).then(answers => {
         console.log(answers.mainAction)
-        switch(answers.mainAction){
+        switch (answers.mainAction) {
             case "POST an Item":
                 break;
             case "BID on an Item":
+                returnItems()
                 break;
             case "EXIT":
                 process.exit();
@@ -26,3 +25,23 @@ function main(){
         }
     });
 }
+
+function returnItems() {
+    connection.query("SELECT * FROM Item", function(err, res) {
+                console.log(res)
+                inquirer.prompt([{
+                        name: "choice",
+                        type: "list",
+                        choices: function(value) {
+                            var choiceArray = [];
+                            for (var i = 0; i < res.length; i++) {
+                                choiceArray.push(res[i].name)
+                            }
+                            return choiceArray;
+                        },
+                        message: "what auction would you like to bid on?"
+                    }.then(function(postres) {
+
+                    })
+                })
+            }
